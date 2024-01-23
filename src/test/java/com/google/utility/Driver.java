296b391f49bebbfc51edd6e7c17e9c902218153f
lounes_hd <zizoulounes@gmail.com>
2024-01-23 12:29:54 -0600

    2st changes

--- src/test/java/com/google/utility/Driver.java
+++ src/test/java/com/google/utility/Driver.java
@@ -40,7 +40,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "52.90.101.17";
+                        String gridAddress = "jenkins.kodif.io/";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
